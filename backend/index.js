d78d93985291ddf9b5702b93ab0144279a61841c
Hector <hectorcortesr04@gmail.com>
2025-07-28 20:43:58 -0600

    The app works. I just need to adapt it to run just with the docker-compose and then in an EC2 instance

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import './server.js'
 import { getClubs, postClub } from './server.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+
+app.use(cors());
+app.use(express.urlencoded())
+app.use(express.json());
 
 // Basic route
 app.get('/', (req, res) => {
@@ -15,8 +21,9 @@ app.get('/data', async (req, res) => {
     res.json(clubs);
 });
 app.post('/set', async (req, res) => {
-    await postClub("asdf, ", "dsvvv", 39889);
-    res.send("It was edited.");
+  const data = req.body;
+  await postClub(data.name, data.city, data.year);
+  res.send("It was edited.");
 })
 
 // Start server
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,89 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clubs, setClubs] = useState([]);
+  const [clubName, setClubName] = useState("");
+  const [city , setCity] = useState("");
+  const [year, setYear] = useState("");
+
+  const callAPIGetClubs = () => {
+    fetch("http://localhost:3001/data")
+    .then((data) => data.json())
+    .then((res) => setClubs(res));
+  }
+
+  const callAPISetClubs = () => {
+
+    fetch("http://localhost:3001/set")
+  }
+  const setForm = () => {
+    const formData = {
+      "name": clubName,
+      "city": city,
+      "year": year,
+    }
+
+    fetch('http://localhost:3001/set', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+  }
+
+  useEffect(() => {
+    callAPIGetClubs();
+  }, []);
 
   return (
-    <>
-      <h1>Prueba Nginx</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-       
+    <div className='windows'>
+    <h1>Mexican Futball Clubs</h1>
+
+    <div className='cont_form'>
+      <form className='form_c' action={setForm} method="post">
+        <label htmlFor="f_name">
+          <span className="f_span">Name:</span>
+          <input type="text" name="name" id="f_name" value={clubName} onChange={(e) => setClubName(e.target.value)}/>
+        </label>
+        <label htmlFor="f_city">
+          <span className="f_span">City:</span>
+          <input type="text" name="city" id="f_city" value={city} onChange={(e) => setCity(e.target.value)}/>
+        </label>
+
+        <label htmlFor="f_y">
+          <span className="f_span">Year founded:</span>
+          <input type="number" name="year_founded" id="f_y" value={year} onChange={(e) => setYear(e.target.value)}/>
+        </label>
+
+        <input type="submit" value="Submit" />
+        
+      </form>
+    </div>
+
+    <div className='cont_data'>
+
+      <div className='grid'>
+        <p>Club name</p>
+        <p>City</p>
+        <p>Year founded</p>
+
+        <p>sdfsfdsdf</p>
+        <p>s dss d d</p>
+        <p>9898</p>
+
+        {clubs.map((ele) => {
+          return (<>
+          <p>{ele["name"]}</p>
+          <p>{ele["city"]}</p>
+          <p>{ele["founded_year"]}</p>
+          </>)
+        })}
+
       </div>
-      
-    </>
+
+    </div>
+    
+    </div>
   )
 }
 
